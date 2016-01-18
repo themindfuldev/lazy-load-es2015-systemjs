2cffe07da57fa830c7e672208fed236e05eda615
Tiago Garcia <tiagoromerogarcia@gmail.com>
2016-01-17 21:18:23 -0800

    adding tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,19 @@
 var gulp = require('gulp');
-// var babel = require('gulp-babel');
-// var mocha = require('gulp-mocha');
+var mocha = require('gulp-mocha');
 var connect = require('gulp-connect');
+require('babel-core/register');
 
 gulp.task('default', ['connect']);
 
-gulp.task('babel', function() {
-  return gulp.src('src/*.js')
-    .pipe(babel())
-    .pipe(gulp.dest('app/js'));
-});
-
-gulp.task('test', ['babel'], function() {
+gulp.task('test', function() {
   return gulp.src('test/*.js')
-    //.pipe(babel())
     .pipe(mocha())
     .on('error', function() {
       gulp.emit('end');
     });
 });
 
-gulp.task('connect', function() {
+gulp.task('connect', ['test'], function() {
   connect.server({
     root: '.'
   });
